5666c1a8c1830ec13a4875c29027dcd0a9681489
Brooks Watson <brooks.watson@appian.com>
2023-01-20 13:43:04 -0500

    more fixes

--- scripts/main.js
+++ scripts/main.js
@@ -28,7 +28,7 @@ function openCity(evt, cityName) {
     // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("navbar");
     for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+      tablinks[i].className = "active";
     }
 
     // Show the current tab, and add an "active" class to the button that opened the tab
